eeea5ff993269fa2d54f2b835c6b9cd3987aead8
Sieun Kim <20210760@dongduk.ac.kr>
2025-05-03 21:18:46 +0900

    [refactor] LoginType → OAuthProvider 분리 및 적용

--- src/main/java/ktb/leafresh/backend/domain/member/domain/entity/enums/LoginType.java
+++ src/main/java/ktb/leafresh/backend/domain/member/domain/entity/enums/LoginType.java
@@ -1,23 +1,5 @@
 package ktb.leafresh.backend.domain.member.domain.entity.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum LoginType {
-    KAKAO, NAVER, GOOGLE;
-
-    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-    public static LoginType from(String value) {
-        for (LoginType type : values()) {
-            if (type.name().equalsIgnoreCase(value)) {
-                return type;
-            }
-        }
-        throw new RuntimeException("지원하지 않는 provider입니다: " + value); // 무조건 예외 잡힘
-    }
-
-    @JsonValue
-    public String toJson() {
-        return name();
-    }
+    LOCAL, SOCIAL;
 }
