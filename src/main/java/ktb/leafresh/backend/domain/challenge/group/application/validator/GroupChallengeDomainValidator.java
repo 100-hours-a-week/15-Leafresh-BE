6f6973e726f30d3e9892b5d27ef0d9d8c70bb4d0
Sieun Kim <20210760@dongduk.ac.kr>
2025-05-04 13:40:28 +0900

    feat: 그룹 챌린지 수정 시 예시 이미지 개수 유효성 검증 추가

--- src/main/java/ktb/leafresh/backend/domain/challenge/group/application/validator/GroupChallengeDomainValidator.java
+++ src/main/java/ktb/leafresh/backend/domain/challenge/group/application/validator/GroupChallengeDomainValidator.java
@@ -2,6 +2,7 @@ package ktb.leafresh.backend.domain.challenge.group.application.validator;
 
 import ktb.leafresh.backend.domain.challenge.group.infrastructure.repository.GroupChallengeCategoryRepository;
 import ktb.leafresh.backend.domain.challenge.group.presentation.dto.request.GroupChallengeCreateRequestDto;
+import ktb.leafresh.backend.domain.challenge.group.presentation.dto.request.GroupChallengeUpdateRequestDto;
 import ktb.leafresh.backend.global.exception.CustomException;
 import ktb.leafresh.backend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,29 @@ public class GroupChallengeDomainValidator {
     private final GroupChallengeCategoryRepository categoryRepository;
 
     public void validate(GroupChallengeCreateRequestDto dto) {
+        if (!dto.endDate().isAfter(dto.startDate())) {
+            throw new CustomException(ErrorCode.INVALID_DATE_RANGE);
+        }
+
+        if (ChronoUnit.DAYS.between(dto.startDate(), dto.endDate()) < 1) {
+            throw new CustomException(ErrorCode.CHALLENGE_DURATION_TOO_SHORT);
+        }
+
+        if (!dto.verificationEndTime().isAfter(dto.verificationStartTime())) {
+            throw new CustomException(ErrorCode.INVALID_VERIFICATION_TIME);
+        }
+
+        if (Duration.between(dto.verificationStartTime(), dto.verificationEndTime()).toMinutes() < 10) {
+            throw new CustomException(ErrorCode.VERIFICATION_DURATION_TOO_SHORT);
+        }
+
+        boolean exists = categoryRepository.findByName(dto.category()).isPresent();
+        if (!exists) {
+            throw new CustomException(ErrorCode.CHALLENGE_CATEGORY_NOT_FOUND);
+        }
+    }
+
+    public void validate(GroupChallengeUpdateRequestDto dto) {
         if (!dto.endDate().isAfter(dto.startDate())) {
             throw new CustomException(ErrorCode.INVALID_DATE_RANGE);
         }
