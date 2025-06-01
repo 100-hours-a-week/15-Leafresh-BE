d53a78bfb90ff56ebe714bef5025ee36575adb1e
Sieun Kim <20210760@dongduk.ac.kr>
2025-06-01 14:29:28 +0900

    chore: sessionId 필드 활성화 (주석 제거)

--- src/main/java/ktb/leafresh/backend/domain/chatbot/infrastructure/client/FakeAiChatbotBaseInfoClient.java
+++ src/main/java/ktb/leafresh/backend/domain/chatbot/infrastructure/client/FakeAiChatbotBaseInfoClient.java
@@ -15,8 +15,8 @@ public class FakeAiChatbotBaseInfoClient implements AiChatbotBaseInfoClient {
         return new AiChatbotBaseInfoResponseDto(
                 "제로웨이스트",
                 List.of(
-                        new AiChatbotBaseInfoResponseDto.Challenge("제로웨이스트 입문", "일상 속 쓰레기를 줄이는 챌린지"),
-                        new AiChatbotBaseInfoResponseDto.Challenge("플라스틱 줄이기", "텀블러, 장바구니 실천하기")
+                        new AiChatbotBaseInfoResponseDto.Challenge("제로웨이스트 입문", "일상 속 쓰레기를 줄이는 챌린지", "ZERO_WASTE", "제로웨이스트"),
+                        new AiChatbotBaseInfoResponseDto.Challenge("플라스틱 줄이기", "텀블러, 장바구니 실천하기", "ZERO_WASTE", "제로웨이스트")
                 )
         );
     }
--- src/main/java/ktb/leafresh/backend/domain/chatbot/infrastructure/client/FakeAiChatbotFreeTextClient.java
+++ src/main/java/ktb/leafresh/backend/domain/chatbot/infrastructure/client/FakeAiChatbotFreeTextClient.java
@@ -16,8 +16,8 @@ public class FakeAiChatbotFreeTextClient implements AiChatbotFreeTextClient {
         return new AiChatbotFreeTextResponseDto(
                 "제로웨이스트",
                 List.of(
-                        new AiChatbotFreeTextResponseDto.Challenge("텀블러 사용하기", "일회용 컵 대신 텀블러 사용 실천하기"),
-                        new AiChatbotFreeTextResponseDto.Challenge("대중교통 이용하기", "자차 대신 버스나 지하철 이용")
+                        new AiChatbotFreeTextResponseDto.Challenge("텀블러 사용하기", "일회용 컵 대신 텀블러 사용 실천하기", "ZERO_WASTE", "제로웨이스트"),
+                        new AiChatbotFreeTextResponseDto.Challenge("대중교통 이용하기", "자차 대신 버스나 지하철 이용", "ZERO_WASTE", "제로웨이스트")
                 )
         );
     }
