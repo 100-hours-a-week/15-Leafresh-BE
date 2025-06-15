119e37bebb1d0c17202c12db4ccbe97e08aa7a5c
Sieun Kim <20210760@dongduk.ac.kr>
2025-06-16 00:08:16 +0900

    feat: 인증 이력을 챌린지별로 그룹핑하여 반환하는 메서드 시그니처 추가

--- src/main/java/ktb/leafresh/backend/domain/challenge/group/infrastructure/repository/GroupChallengeVerificationQueryRepository.java
+++ src/main/java/ktb/leafresh/backend/domain/challenge/group/infrastructure/repository/GroupChallengeVerificationQueryRepository.java
@@ -1,8 +1,10 @@
 package ktb.leafresh.backend.domain.challenge.group.infrastructure.repository;
 
+import ktb.leafresh.backend.domain.challenge.group.presentation.dto.response.GroupChallengeParticipationSummaryDto;
 import ktb.leafresh.backend.domain.verification.domain.entity.GroupChallengeVerification;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface GroupChallengeVerificationQueryRepository {
@@ -11,4 +13,6 @@ public interface GroupChallengeVerificationQueryRepository {
     List<GroupChallengeVerification> findByParticipantRecordId(Long participantRecordId);
 
     Optional<GroupChallengeVerification> findByChallengeIdAndId(Long challengeId, Long verificationId);
+
+    Map<Long, List<GroupChallengeParticipationSummaryDto.AchievementRecordDto>> findVerificationsGroupedByChallenge(List<Long> challengeIds, Long memberId);
 }
