24f69d94d4c2d0e21db2ea03fd336c93ab97f843
Sieun Kim <20210760@dongduk.ac.kr>
2025-09-08 17:39:50 +0900

    fix: TokenBlacklistService 다중 빈 주입 충돌 문제 해결

--- src/main/java/ktb/leafresh/backend/global/security/RedisTokenBlacklistService.java
+++ src/main/java/ktb/leafresh/backend/global/security/RedisTokenBlacklistService.java
@@ -4,6 +4,7 @@ import io.rebloom.client.Client;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 @Service
 @RequiredArgsConstructor
 @Profile("!swagger")
+@Primary
 public class RedisTokenBlacklistService implements TokenBlacklistService {
 
   private final StringRedisTemplate redisTemplate;
